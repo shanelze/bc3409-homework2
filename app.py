2d0076ea7b5a116b7a91efd5a2b444c158f35beb
shanelze <shanelze31@gmail.com>
2024-09-04 13:17:10 +0800

    Added Homework2, Loan Default files, and app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+
+from scipy import stats
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn import linear_model,tree,ensemble,neural_network
+
+#df = pd.read_csv("/content/drive/MyDrive/2024 BC2409/Week 3/german_credit.csv")
